be5be41ccdd36db589cf7da67128c19768917e6e
Sparks <dsparks@proforma.com>
2018-03-30 15:52:35 -0400

    top album list

--- public/main.js
+++ public/main.js
@@ -1,5 +1,16 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
 
-document.addEventListener('DOMContentLoaded', main)
+
+const BASE_URL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=793e56bbeef647bad01344ad6713671e&format=json"
+
+angular
+  .module("lastfmApp", [])
+  .controller("mainController", ["$scope", "$http",($scope, $http) => {
+
+    $http({
+      method: "GET",
+      url: BASE_URL,
+    }).then(response =>{
+      console.log(response.data);
+      $scope.artists = response.data.artists.artist;
+    })
+  }])
